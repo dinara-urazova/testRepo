5f0f445a5cd71bc367d8c7b4b6a9b9874fa39493
Dinara Urazova <dinara.urazova88@gmail.com>
2025-02-05 17:11:19 +0500

    register form upd, comments, envvar - register: pwd_validation (‘repeat password’) added - register: username_validation (min and max length) added - fix env var (via dotenv) - comments added for better understanding

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
+    POSTGRESQL_DATABASE=os.getenv('POSTGRESQL_DATABASE')
+    POSTGRESQL_USERNAME=os.getenv('POSTGRESQL_USERNAME')
+    POSTGRESQL_PASSWORD=os.getenv('POSTGRESQL_PASSWORD')
+    POSTGRESQL_HOSTNAME=os.getenv('POSTGRESQL_HOSTNAME')
+    POSTGRESQL_PORT=os.getenv('POSTGRESQL_PORT')
--- forms.py
+++ forms.py
@@ -1,13 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    submit = SubmitField("Sign In")
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators
 
 class RegisterForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
+    username = StringField("Username", [validators.Length(min=4, max=25, message="Username should be between 4-25 characters")])
+    password = PasswordField(
+        "New Password",
+        [
+            validators.DataRequired(),
+            validators.EqualTo("confirm", message="Passwords must match"),
+        ],
+    )
+    confirm = PasswordField("Repeat Password")
     submit = SubmitField("Register")
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", [validators.DataRequired()])
+    password = PasswordField("Password", [validators.DataRequired()])
+    submit = SubmitField("Sign In")
